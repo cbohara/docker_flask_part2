7b151338a557cb089e3572cf8aaf3f599e3863ed
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-05-13 08:24:00 -0700

    adding Flask API

--- app/main.py
+++ app/main.py
@@ -1,10 +1,33 @@
 from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import identity, authenticate
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hey I'm using Docker!"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.secret_key = "charlie"
+api = Api(app)
+jwt = JWT(app, authenticate, identity)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(StoreList, "/stores")
+api.add_resource(UserRegister, "/register")
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=80)
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
