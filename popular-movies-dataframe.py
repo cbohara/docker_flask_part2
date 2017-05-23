c58863cfbcb566930d884e3538f9e45161c04f68
cohara <cohara@algebraixdata.com>
2017-05-23 11:23:28 -0700

    debugging to work with python3

--- popular-movies-dataframe.py
+++ popular-movies-dataframe.py
@@ -3,30 +3,31 @@ from pyspark.sql import Row
 from pyspark.sql import functions
 
 def loadMovieNames():
+    """Load in data into dictionary of movieID and movie title"""
     movieNames = {}
-    with open("data/ml-100k/u.item") as f:
+    with open("data/ml-100k/u.item", encoding='ascii', errors='ignore') as f:
         for line in f:
             fields = line.split('|')
-            movieNames[int(fields[0])] = fields[1]
+            movieNames[int(fields[0])] = fields[1].encode("utf8")
     return movieNames
 
-# Create a SparkSession (the config bit is only for Windows!)
+
 spark = SparkSession.builder.appName("PopularMovies").getOrCreate()
 
-# Load up our movie ID -> name dictionary
+# load up our movie ID -> name dictionary
 nameDict = loadMovieNames()
 
-# Get the raw data
-lines = sc.textFile("file:///home/cohara/github/taming_big_data_spark/data/ml-100k/u.data")
-# Convert it to a RDD of Row objects
+# import unstructured data into RDD 
+lines = spark.sparkContext.textFile("file:////home/cohara/github/taming_big_data_spark/data/ml-100k/u.data")
+# convert it to a RDD of Row objects with a single column (movieID)
 movies = lines.map(lambda x: Row(movieID =int(x.split()[1])))
-# Convert that to a DataFrame
+# convert RDD to a DataFrame
 movieDataset = spark.createDataFrame(movies)
 
-# Some SQL-style magic to sort all movies by popularity in one line!
+# some SQL-style magic to sort all movies by popularity in one line!
 topMovieIDs = movieDataset.groupBy("movieID").count().orderBy("count", ascending=False).cache()
 
-# Show the results at this point:
+# show the results at this point:
 
 #|movieID|count|
 #+-------+-----+
@@ -36,14 +37,14 @@ topMovieIDs = movieDataset.groupBy("movieID").count().orderBy("count", ascending
 
 topMovieIDs.show()
 
-# Grab the top 10
+# take the top 10
 top10 = topMovieIDs.take(10)
 
-# Print the results
+# print the results
 print("\n")
 for result in top10:
     # Each row has movieID, count as above.
     print("%s: %d" % (nameDict[result[0]], result[1]))
 
-# Stop the session
+# stop the session
 spark.stop()
